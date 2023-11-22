fbdd88c1052af0a5a7f7a90b378b4417f49338e6
MrMax01 <massimo.mauro00@gmail.com>
2023-11-22 15:12:52 +0100

    try to fix

--- src/redux/reducer/clientReducer.js
+++ src/redux/reducer/clientReducer.js
@@ -3,6 +3,7 @@ import { GET_CLIENTS } from "../actions";
 const initialState = {
   content: null,
 };
+
 const clientReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CLIENTS:
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,7 +3,6 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
 import storage from "redux-persist/lib/storage";
-import clientReducer from "../reducer/clientReducer";
 import userReducer from "../reducer/userReducer";
 
 const persistConfig = {
@@ -11,9 +10,9 @@ const persistConfig = {
   storage,
   whitelist: [],
 };
+
 const rootReducer = combineReducers({
   userToken: userReducer,
-  clients: clientReducer,
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
